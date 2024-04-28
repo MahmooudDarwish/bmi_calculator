06a91792499c17243dbcdb8da5ef07d784109ecb
Mahmoud Darwish <mahmoudsaaddarwish1@gmail.com>
2024-04-29 01:00:49 +0300

    add route paths done

---
+++ lib/core/services/navigation/route_path.dart
@@ -0,0 +1,7 @@
+class RoutePath {
+  RoutePath._();
+  static const String signInPage = "/sign-in";
+  static const String bmiEntriesPage = "/bmi-entries";
+  static const String bmiFormPage = "/bmi-form";
+  static const String bmiDetailsPage = "/bmi-details";
+}
